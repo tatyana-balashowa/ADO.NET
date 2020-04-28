6ed233cd44087b70a98be72f1dccc7d50e98b3be
tatyana-balashowa <63149624+tatyana-balashowa@users.noreply.github.com>
2020-04-28 16:34:37 +0400

    UsersAndAwards.ConsolePL

---
+++ MyLogic.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Users.BLL;
+using Users.Entities;
+
+namespace Users.ConsolePL
+{
+    class MyLogic
+    {
+        private static IUserLogic userLogic = new UserLogic();
+        private static AwardLogic awardLogic = new AwardLogic();
+        public static void AddUser()
+        {
+            Console.WriteLine("Имя пользователя:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Дата рождения:");
+            try
+            {
+                if (DateTime.TryParse(Console.ReadLine(), out var dateOfBirth))
+                {
+                    Console.WriteLine("Возраст:");
+                    if (int.TryParse(Console.ReadLine(), out var age))
+                    {
+                        var newUser = new User()
+                        {
+
+                            Name = name,
+                            DateOfBirth = dateOfBirth,
+                            Age = age
+                        };
+                        userLogic.AddUser(newUser);
+                        Console.WriteLine("Пользователь добавлен!");
+                    }
+                    else
+                    {
+                        throw new Exception("No correctly format of User's age");
+                    }
+                } else
+                {
+                    throw new Exception("No correctly format of User's dateOfBirth");
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Некорректные данные пользователя!");
+            }
+        }
+        public static void GetUsers()
+        {
+            try
+            {
+                var result = userLogic.GetUsers();
+                if (result.Any())
+                {
+                    foreach (var item in result)
+                    {
+                        Console.WriteLine(item.ToString());
+                    }
+                } else
+                {
+                    throw new Exception("Empty!");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Нет пользователей!");
+            }
+        }
+        public static void RemoveUser()
+        {
+            Console.WriteLine("Укажите ID пользователя:");
+            try
+            {
+                if (int.TryParse(Console.ReadLine(), out var indexUser) && userLogic.GetNeedUsers(indexUser).Any())
+                {
+                    userLogic.RemoveUser(indexUser);
+                    Console.WriteLine("Пользователь удален!");
+                } else
+                {
+                    throw new Exception("No correctly operation");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Пользвателя с таким ID не существует!");
+            }
+        }
+        public static void AddAward()
+        {
+            Console.WriteLine("ID пользователя:");
+            try
+            {
+                if (int.TryParse(Console.ReadLine(), out var indexUser) && userLogic.GetNeedUsers(indexUser).Any())
+                {
+                    Console.WriteLine("Название награды:");
+                    string Title = Console.ReadLine();
+                    awardLogic.AddAward(indexUser, Title);
+                    Console.WriteLine("Награда добавлена!");
+                } else
+                {
+                    throw new Exception("No correctly operation!");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Некорректный ID!");
+            }
+        }
+        public static void GetAwardsByUser()
+        {
+            Console.WriteLine("ID пользователя:");
+            try
+            {
+
+                if (int.TryParse(Console.ReadLine(), out var indexUser) && userLogic.GetNeedUsers(indexUser).Any())
+                {
+                    var result = awardLogic.GetAwardsByUser(indexUser);
+                    if (result.Any())
+                    {
+                        foreach (var item in result)
+                        {
+                            Console.WriteLine(item.ToString());
+                        }
+                    } else
+                    {
+                        throw new Exception("No awards!");
+                    }
+                } else
+                {
+                    throw new Exception("No correctly operation!");
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Некоректный ID/у пользователя нет наград");
+            }
+        }
+        public static void RemoveAward()
+        {
+            Console.WriteLine("ID пользователя:");
+            try
+            {
+                if (int.TryParse(Console.ReadLine(), out var indexUser) && userLogic.GetNeedUsers(indexUser).Any())
+                {
+                    Console.WriteLine("Название награды:");
+                    string Title = Console.ReadLine();
+                    if (awardLogic.GetNeedAwards(indexUser, Title).Any())
+                    {
+                        awardLogic.RemoveAward(indexUser, Title);
+                    } else
+                    {
+                        throw new Exception("No correctly Title!");
+                    }
+                } else
+                {
+                    throw new Exception("No correctly IDUser!");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Некорректные данные пользователя/награды!");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Users.ConsolePL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Выберите одно из следующих действий:"
+                                 + Environment.NewLine +
+                                 "1 - Добавить пользователя"
+                                 + Environment.NewLine +
+                                 "2 - Просмотреть список пользователей"
+                                 + Environment.NewLine +
+                                  "3 - Удалить пользователя"
+                                  + Environment.NewLine +
+                                  "4 -Добавить награду"
+                                  + Environment.NewLine +
+                                  "5 - Просмотреть перечень наград пользователя"
+                                  + Environment.NewLine +
+                                  "6 - Удалить награду");
+                var action = Console.ReadLine();
+                switch (action)
+                {
+                    case "1":
+                        MyLogic.AddUser();
+                        break;
+                    case "2":
+                        MyLogic.GetUsers();
+                        break;
+                    case "3":
+                        MyLogic.RemoveUser();
+                        break;
+                    case "4":
+                        MyLogic.AddAward();
+                        break;
+                    case "5":
+                        MyLogic.GetAwardsByUser();
+                        break;
+                    case "6":
+                        MyLogic.RemoveAward();
+                        break;
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
