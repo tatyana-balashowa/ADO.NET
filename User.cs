82e73b00d28d187e8b08a36eb7cb156567bbcbee
tatyana-balashowa <63149624+tatyana-balashowa@users.noreply.github.com>
2020-04-28 16:32:08 +0400

    UsersAndAwards.Entities

---
+++ User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Users.Entities
+{
+    public class User
+    {
+        public int  ID { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int Age { get; set; }
+        public override string ToString()
+        {
+            return $"{ID} {Name} {DateOfBirth.Date} {Age}";
+        }
+
+    }
+}
